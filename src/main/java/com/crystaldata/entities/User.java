9f68cb6e062064ebc2966551c9107e0f8076aa5f
Henio Junior <henioajunior@gmail.com>
2021-02-15 09:23:21 -0300

    Database seeding

--- src/main/java/com/crystaldata/entities/User.java
+++ src/main/java/com/crystaldata/entities/User.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 //@Entity
 //@Table(name = "tb_users")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_genre (name) VALUES ('Action');
+INSERT INTO tb_genre (name) VALUES ('Comedy');
+INSERT INTO tb_genre (name) VALUES ('Drama');
+INSERT INTO tb_genre (name) VALUES ('Fantasy');
